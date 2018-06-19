8908ead38a41a08da99d4e9649f772b7d2ef7ad5
John Gruber <john.gruber@gitbit.org>
2018-06-19 12:07:58 -0400

    added nav to header

--- src/header.js
+++ src/header.js
@@ -4,9 +4,14 @@ class Header extends React.Component {
   render() {
     return (
       <header className="nav-top space-between">
-        <a className="align-center side-margins">
+        <a className="align-center side-margins" href="#">
           <h1 className="no-margin light">GitBit</h1>
         </a>
+        <div>
+          <a href="#connectors" className="side-margins light">Connectors</a>
+          <a href="#workflows" className="side-margins light">Workflows</a>
+          <a href="#channels" className="side-margins light">Channels</a>
+        </div>
       </header>
     )
   }
