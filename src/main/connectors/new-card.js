b9b7f21d87a0b39b7a6b2b524cde425600d2b84e
John Gruber <john.gruber@gitbit.org>
2018-06-24 14:14:42 -0400

    passport is dumb

--- src/main/connectors/new-card.js
+++ src/main/connectors/new-card.js
@@ -1,6 +1,24 @@
 const React = require('react')
+const uuid = require('uuid/v4')
+const {ws} = require('../../lib/index.js')
 
 class ConnectorNewCard extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state={id:uuid()}
+
+    ws.on('WORKSPACE_UPDATED', ({workspace}) => {
+      console.info('ConnectorNewCard received WORKSPACE_UPDATED')
+      console.info(this.state.id)
+      console.info(workspace.connectors)
+      console.info(this.state.authWindow)
+      if (workspace.connectors[this.state.id] && this.state.authWindow) {
+        this.state.authWindow.close()
+        this.setState({authWindow: null})
+      }
+    })
+  }
+
   handleChange(event) {
     const connector = Object.assign({}, this.props.connector)
     connector[event.target.name] = event.target.value
@@ -8,7 +26,9 @@ class ConnectorNewCard extends React.Component {
   }
 
   auth() {
-    window.open('/auth/twitter', 'newwindow', 'width=300,height=250')
+    this.setState({
+      authWindow: window.open(`/auth/twitter?connectorId=${this.state.id}`, 'newwindow', 'width=500,height=500')
+    })
   }
 
   render() {
