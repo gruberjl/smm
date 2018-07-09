bf9c58d2ef4b1b8be8b6e7a82be9d29348cae71c
John Gruber <john.gruber@gitbit.org>
2018-07-09 14:25:15 -0400

    fixed workflow bug

--- src/lib/store/channel-reducer.js
+++ src/lib/store/channel-reducer.js
@@ -1,7 +1,10 @@
+let workflows = []
+
 const channel = (oldState={}, action) => {
   if (action.type=='WORKSPACE_CHANGE') {
     const state = Object.assign({}, oldState)
     const channels = action.docs.filter((doc) => doc.docType=='channel')
+    workflows = action.docs.filter((doc) => doc.docType=='workflow')
     channels.forEach(channel => {
       if (!state[channel.dbName]) state[channel.dbName] = {docs:[]}
     })
@@ -11,7 +14,11 @@ const channel = (oldState={}, action) => {
   if (action.type=='CHANNEL_CHANGE') {
     const state = Object.assign({}, oldState)
     const channel = action.channel
-    state[channel.dbName].docs = action.docs
+    const docs = action.docs.map(doc => {
+      doc.workflow = workflows.find(w => w._id == doc.workflow._id)
+      return doc
+    })
+    state[channel.dbName].docs = docs
     return state
   }
 
